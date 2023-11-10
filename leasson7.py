56e656184c7ee89587360b2e0677b5fc973d0fd7
luckyamina <144383353+luckyamina@users.noreply.github.com>
2023-11-10 18:46:58 +0400

    Add files via upload

---
+++ leasson7.py
@@ -0,0 +1,13 @@
+#homework 1
+leader_names = ["gabrieli","dato janezashvili"," dato Tyeshelashvili", "filiashvili"]
+print(leader_names[1])
+
+#homweork 2
+name =[ "tato chogovade", "luka chxitunidze", "luka kechexmadze", "noi comaia", "cotne sartania", "giorgi gorgiladze", "dachi abashidze", "luka suxiashvili", "luka siradze", "misho chubinidze", "gigi gabitadze", "ako mitiashvili", "dito muladze", "omiko mshvildadze", "nika bregadze"]
+
+print(name[6:12])
+
+#homework 3 
+fruits = ["vashli", "gargali", "kivi", "atami"]
+fruits[3] = "banani"
+print(fruits)
